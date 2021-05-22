d98d8bf7aefdba5de1289532473630a8cbb6c656
Mngz <mngz636@gmail.com>
2021-05-22 15:41:29 +0200

    Update PHPMailerTest.php

--- PHPMailerTest.php
+++ PHPMailerTest.php
@@ -3186,13 +3186,13 @@ EOT;
  
   <form name="phpmailer_unit" action=__FILE__ method="get">
   <input type="hidden" name="submitted" value="1"/>
-  From Address: <input type="text" size="50" name="mail_from" value="<?php echo get("mail_from"); ?>"/>
+  From Address: <input type="text" size="50" name="mail_from" />
   <br/>
-  To Address: <input type="text" size="50" name="mail_to" value="<?php echo get("mail_to"); ?>"/>
+  To Address: <input type="text" size="50" name="mail_to" />
   <br/>
-  Cc Address: <input type="text" size="50" name="mail_cc" value="<?php echo get("mail_cc"); ?>"/>
+  Cc Address: <input type="text" size="50" name="mail_cc" />
   <br/>
-  SMTP Hostname: <input type="text" size="50" name="mail_host" value="<?php echo get("mail_host"); ?>"/>
+  SMTP Hostname: <input type="text" size="50" name="mail_host" />
         <br/>
   SMTP Username: <input type="text" size="50" name="mail_username" value=""/>
         <br/>
